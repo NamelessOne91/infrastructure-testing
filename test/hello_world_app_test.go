a7a41e8c8b3d6e66713356170a30694cc0fd2472
Paolo Invernizzi <paolo.invernizzi@lightcast.com>
2023-09-22 18:27:12 +0200

    Add hello world app unit test

---
+++ test/hello_world_app_test.go
@@ -0,0 +1,32 @@
+package test
+
+import (
+	"testing"
+	"time"
+
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestHelloWorldAppUnit(t *testing.T) {
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../examples/hello-world-app",
+	}
+	// cleanup
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+	validateHelloWorldApp(t, terraformOptions)
+}
+
+func validateHelloWorldApp(t *testing.T, opts *terraform.Options) {
+	url := terraform.Output(t, opts, "url")
+	http_helper.HttpGetWithRetry(t,
+		url,             // URL to test
+		nil,             // TLS config
+		200,             // Expected status
+		"Hello, World!", //Expected body
+		10,              // Max retries
+		3*time.Second,   // Time between retries
+	)
+}
